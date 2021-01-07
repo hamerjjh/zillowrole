91ae31ff973965b07777eb514a066ae362f4ca4d
Jonathan Hamer <jonathanhamer@Jonathans-Air.attlocal.net>
2021-01-07 13:43:36 -0500

    created a navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Gallery from "./components/Gallery"
+import Gallery from "./components/Gallery";
+import Navbar from './components/Navbar';
 
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
 
         <div>
+        <Navbar />
         <Gallery />
         </div>
     )
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+
+class Navbar extends Component {
+
+  render () {
+    return (
+
+        <div>
+          <h1>Welcome to Zillow </h1>
+          <h2>The famous house from Home Alone is now the newest rental property on the market!</h2>
+          <h3>671 Lincoln Ave, Winnetka, IL 60093</h3>
+        </div>
+    )
+  }
+}
+
+export default Navbar;
