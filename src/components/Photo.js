f540e34ff0206015b7f760d6e9ea49945bfa5cdd
Jonathan Hamer <jonathanhamer@Jonathans-Air.attlocal.net>
2021-01-07 13:52:22 -0500

    create picture frame css to contain photo img and photo description

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import "./Photo.css"
 
 const Photo = ({photo}) => {
     const {index, photoLink, description} = photo;
     return (
-        <div id={`card-${index}`}>
+        <div id={`card-${index}`} className="card">
             <img src={photoLink} alt="" />
             <div>
                 <h2>{description}</h2>
