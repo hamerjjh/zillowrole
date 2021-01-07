bd0fa7c596719535f44f69f9d8ad7dd2b41e695d
Jonathan Hamer <jonathanhamer@Jonathans-Air.attlocal.net>
2021-01-07 13:23:59 -0500

    created a datajs for the photo details

---
+++ src/Data.js
@@ -0,0 +1,35 @@
+const Data = {
+    "galleryList": [
+        {
+            "index":0,
+            "photoLink":"https://rdcnewscdn.realtor.com/wp-content/uploads/2015/11/home-alone-house.jpg",
+            "description": "Wonderful Three bedroom Rental Property"
+        },
+        {
+            "index":1,
+            "photoLink":"https://s3-prod.chicagobusiness.com/s3fs-public/styles/width_792/public/2020-02/5_one_bennett_park_master_bedroom.jpg",
+            "description": "Light-filled Master Bedroom"
+        },
+        {
+            "index":2,
+            "photoLink":"https://i.pinimg.com/originals/e5/fe/9d/e5fe9ded41e241f0f9aabe11eb8cdc99.jpg",
+            "description": "Private Guest Quarters"
+        },
+        {
+            "index":3,
+            "photoLink":"https://cdn.homedit.com/wp-content/uploads/2014/07/modern-bedroom-design-chandelier-and-large-headboard.jpg",
+            "description": "Thrid Bedroom with Outstandning Closet Space"
+        },
+        {
+            "index":4,
+            "photoLink":"https://www.bostonmagazine.com/wp-content/uploads/sites/2/2020/02/theater-3.jpg",
+            "description": "Spacious Home Theater For The Whole Family"
+        },
+        {
+            "index":5,
+            "photoLink":"https://i.pinimg.com/originals/ad/d2/bd/add2bd1f241d44511e82df16acb7c300.jpg",
+            "description": "Lovely Indoor Pool Area, Great for Entertaining"
+        }
+    ]
+}
+export default Data;
